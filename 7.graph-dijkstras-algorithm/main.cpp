917219269f8db80759ee01fb8cb237335c8d07c5
Mersed Kahrimanovic <kahrimanovic.mersed@gmail.com>
2019-05-07 06:46:57 +0200

    Finished implementation of djikstra's algo.

--- 7.graph-dijkstras-algorithm/main.cpp
+++ 7.graph-dijkstras-algorithm/main.cpp
@@ -8,6 +8,13 @@
  * As you can see, usage of breadth first search for weighted 
  * graphs is not possible, due to the fact that it would find the path with
  * fewest segments (which maybe is not shortest path from start to finish)
+ *
+ * Within the implementation bellow, empty string is indication of non existance
+ * of the node. The INT_MAX, represent infinite for the wieght of the edge.
+ * This is proof of concept only, and for better understanding of algo more
+ * complex data structures (vectors, hashmaps) are used. In order for Djikstra's
+ * to be optimal - from perspective of space and time complexity - everything
+ * bellow can be implemented with arrays only.
  */
 
 #include <iostream>
@@ -37,6 +44,8 @@ void populateGraph(GraphMap &graph) {
 }
 
 void dipslayGraph(GraphMap &graph) {
+    std::cout << "Graph defined: " << std::endl;
+    std::cout << "------------------" << std::endl;
     GraphMap::iterator it = graph.begin();
     for(; it != graph.end(); it++) {
         ChildNodesMap::iterator iter = it->second.begin();
@@ -46,74 +55,74 @@ void dipslayGraph(GraphMap &graph) {
         }
         std::cout << "}" << std::endl;
     }
+
+    std::cout << std::endl << std::endl;
 }
 
-template <class T>
-void displayList(std::list<T> &v) {
-    std::cout << "{";
-    auto it = v.begin();
-    for(; it != v.end(); it++) {
-        std::cout << *it << ",";
+std::string findLowestCostNode(ChildNodesMap &costs, std::vector<std::string> &processed) {
+    ChildNodesMap::iterator it = costs.begin();
+    std::string lowestCostNode = "";
+    int lowestCost = INT_MAX;
+
+    std::cout << "Lowest cost defined: " << lowestCost << std::endl;
+    for(; it != costs.end(); it++) {
+        std::cout << "Comparation node and price: " << it->first << " ----- " << it->second << std::endl;
+        if (
+            it->second < lowestCost &&
+            std::find(processed.begin(), processed.end(), it->first) == processed.end()
+        ) {
+            std::cout << "findLowestCostNode condition fullfiled " << it->first << std::endl;
+            lowestCost = it->second;
+            lowestCostNode = it->first;
+        }
     }
-    std::cout << "}" << std::endl;
+
+    return lowestCostNode;
 }
 
-void insertToQueue(std::list<std::string> &list, std::vector<std::string> &v) {
-    auto it = v.begin();
-    for(; it != v.end(); it++) {
-        list.push_back(*it);
-    }
-}
 
 int main(int argc, char** argv) {
+    // Define hashmap for graph
     GraphMap graph;
     populateGraph(graph);
     dipslayGraph(graph);
 
+    // Define hashmap for costs
+    ChildNodesMap costs;
+    costs["a"] = graph["start"]["a"];
+    costs["b"] = graph["start"]["b"];
+    costs["c"] = INT_MAX;
+    costs["d"] = INT_MAX;
+    costs["finish"] = INT_MAX;
 
+    // Define hashmap for parents
+    std::map<std::string, std::string> parents;
+    parents["a"] = "start";
+    parents["b"] = "start";
+    parents["c"] = "";
+    parents["d"] = "";
+    parents["finish"] = "";
 
-    /*
-    list<string> queue{};
-    vector<string> searched{};
+    // Define vector for processed
+    std::vector<std::string> processed;
 
-    // Creating a graph using hashmap (simple implementation)
-    graph.insert({ "you", {"alice", "bob", "claire"} });
-    graph.insert({ "bob", {"anuj", "peggy", "you"} });
-    graph.insert({ "alice", {"peggy", "dude", "you"} });
-    graph.insert({ "claire", {"thom", "jonny"} });
-    graph.insert({ "anuj", {"dude"} });
-    graph.insert({ "peggy", {} });
-    graph.insert({ "thom", {} });
-    graph.insert({ "jonny", {} });
+    // Apply djikstra
+    std::string node = findLowestCostNode(costs, processed);
+    while(!node.empty()) {
+        std::cout << "Inside " << node << std::endl;
+        ChildNodesMap neighbours = graph[node];
+        ChildNodesMap::iterator it = neighbours.begin();
+        int cost = costs[node];
 
-    string start = "you";
-    string search = "dude";
-    queue.push_back(start);
-    // breadth first implementation
-    // searching a value from start using shortest way
-    // in our example we are starting from "you" and we are searching "dude"
-    while(!queue.empty()) {
-        if(DEBUG) {
-            cout << "Queue: ";
-            displayList(queue);
-        }
-
-        string temp = queue.front();
-        queue.pop_front();
-        if (std::find(searched.begin(), searched.end(), temp) == searched.end()) {
-            searched.push_back(temp);
-            if(temp == search) {
-                cout << "Found value: " << temp;
-                cout << endl;
-                break;
+        for(; it != neighbours.end(); it++) {
+            int newCost = cost + it->second;
+            if(costs[it->first] > newCost) {
+                costs[it->first] = newCost;
+                parents[it->first] = it->first;
             }
-
-
-            insertToQueue(queue, graph[temp]);
         }
+        processed.push_back(node);
+        node = findLowestCostNode(costs, processed);
     }
-    */
-
-    std::cout << "Testing here additionally" << std::endl;
 }
 
